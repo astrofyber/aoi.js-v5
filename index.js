74efd2c2943de38b033aff6f653c94d67b6d6bf7
Fyber <78761344+CallMeFyber@users.noreply.github.com>
2022-10-03 20:45:47 +0300

    bot status command/welcome system/more events ++

--- index.js
+++ index.js
@@ -1,23 +1,44 @@
-const aoijs = require("aoi.js")
+// AOI.JS VERSION 5 - @ CallMeFyber
+const aoijs = require("aoi.js") // npm i aoi.js@5
 
 const bot = new aoijs.Bot({
-token: "ΤΟΚΕΝ", //Discord Bot Token - Repl.it --> process.env.TOKEN
+token: "ΤΟΚΕΝ", //Discord Bot Token - [ If you use Repl.it --> process.env.TOKEN ]
 prefix: "PREFIX", //Discord Bot Prefix
 intents: ["GUILDS", "GUILD_MESSAGES"] //Discord Intents, you can change it at "all"
 })
 
 //Events
 
-bot.onMessage()
+bot.onLeave();
+bot.onJoin();
+bot.onMessage();
+bot.onUserUpdate();
+bot.onChannelDelete();
+bot.onChannelCreate();
+bot.onChannelUpdate();
+bot.onRoleCreate();
+bot.onRoleDelete();
+bot.onMessageUpdate();
+bot.onMessageDelete();
+bot.onInteractionCreate();
 
-//Command Example
 
-bot.command({
-name: "ping",
-code: `$ping ms`
+//Variables
+
+bot.variables({
+  site: "yoursite.com",
 })
 
-// You can aslo add variables if you want.
+
+// Bot Status
+
+bot.status({
+    text: 'Github @CallMeFyber',
+    type: 'PLAYING',
+    status: 'online',
+    time: 15
+});
+
 
 //Ready Event
 
@@ -27,13 +48,38 @@ bot.readyCommand({
  
 })
 
+
+// Credits Log
+
 console.log("Main index.js file by https://github.com/FyberMods")
 
-//Command Handler loader
+
+// Welcome System 
+
+bot.joinCommand({ 
+ channel:"ChannelID", 
+ code: <@$authorID> Welcome!`
+`})
+
+// Command Example
+
+
+//Command Example
+
+bot.command({
+name: "ping",
+code: `$ping ms`
+})
+
+
+//Command Handler loader - Default Option: OFF
 
 /*
 const loader = new aoijs.LoadCommands(bot);
 loader.load(bot.cmd,'./commands/') 
 */
 
-// Template with command handler loader - FyberDev ( v5 version! )
+// Template with command handler loader - CallMeFyber ( v5 version! )
+
+
+
